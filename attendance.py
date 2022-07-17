f5177b22a9697d0dbc5325105b140b790dca1736
sajal-du <sajal.mcs21@cs.du.ac.in>
2022-07-17 09:05:55 +0000

    Add base layouts and update dependencies

--- attendance.py
+++ attendance.py
@@ -1,7 +1,9 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def main():
-    return flask.render_template("index.jinja.html")
+    return flask.render_template("dba_init_gj.html")
+    # return flask.render_template("index.jinja.html")
