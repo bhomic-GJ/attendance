1c108db83c9ee07a930749c1a50f46aca18ecb1c
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2022-07-20 18:18:27 +0000

    Add dependencies for QRCode scanning

---
+++ static/scripts/attendance.js
@@ -0,0 +1,20 @@
+async function fetchData (url, options = {}) 
+{
+    try {
+        if(options) {
+            if(options.method == "POST" && typeof options.body == "object") {
+                let bodydata = new FormData();
+                for(const property of Object.getOwnPropertyNames(options.body))
+                    bodydata.append(property, options.body[property]);
+                options.body = bodydata;
+            }
+        }
+        let response = await fetch(url, options);
+        console.log(response.status);
+        let data = await response.json();
+        return data;
+    }
+    catch(reason) {
+        console.error(reason);
+    }
+}
