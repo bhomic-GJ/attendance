6285d82f4ccac719443fe457ca20515f1cb99fbc
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2022-07-24 17:41:13 +0000

    Link blueprints for functionality

--- attendance.py
+++ attendance.py
@@ -3,54 +3,111 @@ import time
 
 import flask
 import dotenv
+from flask_httpauth   import HTTPTokenAuth
+from flask_session    import Session
+from flask_socketio   import SocketIO
 from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPTokenAuth
 
-from src import api, utils
+from src import api, routes, utils
 
 # Load the environment variables specific to the app.
 dotenv.load_dotenv()
 
 app = flask.Flask(__name__)
 
-app.config['SECRET_KEY']              = os.environ.get('SECRET_KEY', 'wow_so_secret')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config.update(
+    SECRET_KEY                     = os.environ.get('SECRET_KEY', 'so_secret_much_wow'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False,
+    SQLALCHEMY_DATABASE_URI        = os.environ.get('DB_URL'),
+    SESSION_TYPE                   = 'filesystem',
+    SESSION_USE_SIGNER             = True
+)
 
-db     = SQLAlchemy(app)
-auth   = HTTPTokenAuth(scheme='Bearer')
-tokens = {}
+db       = SQLAlchemy(app)
+socketio = SocketIO(app)
+auth     = HTTPTokenAuth(scheme='Bearer')
+# Initialize a filesystem based server-side session
+Session(app)
+
+tokens   = {}
+
+engine   = db.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], { 'echo': False })
+database = utils.database.get_instance(engine)
+
+# Initialize the code cache pool for generating new organization codes.
+org_codes = utils.CodeGenerator(database.get_organization_codes())
+
+@app.template_filter()
+def field(date, fmtstr):
+    return utils.format_date(date, fmtstr)
+
+@app.before_request
+def prepare_user():
+    flask.g.user = None
+    session_token = flask.session.get('user_token', None)
+    if session_token and session_token in tokens:
+        flask.g.user = tokens.get(session_token, None)
+    elif session_token:
+        flask.session.pop('user_token')
 
 @auth.verify_token
 def verify_token(token):
     for _token, data in tokens.items():
-        if time.time() - data['timestamp'] > 24 * 3600:
+        if int(time.time()) - data['timestamp'] > 24 * 3600:
             del tokens[_token]
     print(tokens)
     if token in tokens:
         return tokens[token]
 
-engine   = db.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], { 'echo': True })
-database = utils.database.get_instance(engine)
+@auth.get_user_roles
+def get_user_roles(user):
+    if 'role' not in user:
+        user['role'] = database.get_user_role(user['ID'])
+    return user['role']
 
+# Register the standard endpoints.
 app.register_blueprint(
-    api.create_blueprint(auth, tokens, database)
+    routes.create_blueprint(
+        auth, tokens, database,
+        socketio=socketio,
+        org_codes=org_codes
+    )
 )
+# Register the API endpoint.
+app.register_blueprint(
+    api.create_blueprint(
+        auth, tokens, database,
+        socketio=socketio,
+        org_codes=org_codes,
+    )
+)
+
+@app.route("/attendance")
+def attendance():
+    return flask.render_template("ui/attendance.html.jinja")
 
 @app.route("/QR")
 def qr():
     return flask.render_template("Qr2.html")
 
-@app.route("/login")
-def login():
-    return flask.render_template("ui/login.html.jinja")
+@app.route("/register")
+def register():
+    return flask.render_template("ui/register.html.jinja")
 
 @app.route("/")
-def main():
+def index():
     # if flask.request.args.get('generate', None) is not None:
     #     qrpath = utils.qrcode.generate_qr("static/qr1.png", "https://github.com")
     # else:
     #     qrpath = None
-
-    return flask.render_template(
-        "ui/home_existing_user.html.jinja"
-    )
+    user = auth.current_user() or flask.g.user
+    print(user)
+    print()
+    if user:
+        return flask.render_template(
+            "ui/home_existing_user.html.jinja",
+            current_user=user,
+            user=database.get_user_by_id(user['ID'])
+        )
+    else:
+        return flask.render_template("ui/home_new_user.html.jinja", current_user=user)
