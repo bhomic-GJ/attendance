263c7865e5ca113320d1978410c4d18e566b75d5
bhomic-GJ <bhomic.mcs21@cs.du.ac.in>
2022-07-27 13:42:06 +0000

    Add routes for testing

--- src/utils/qrcode.py
+++ src/utils/qrcode.py
@@ -4,9 +4,9 @@ import qrcode
 
 from . import new_uuid
 
-def generate_qr(string, path, filename = None):
+def generate_qr(string, path, prefix, filename = None):
     os.makedirs(path, exist_ok=True)
     c = qrcode.make(string)
     path = os.path.join(path, f"{filename or new_uuid()}.png")
-    c.save(path)
+    c.save(os.path.join(prefix, path))
     return path
